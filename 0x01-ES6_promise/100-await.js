7ca401390e11bc93f533847c2e1629c003fd583a
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-25 00:34:36 +0300

    async...await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  const obj = { photo: null, user: null };
+  
+  try {
+    obj.photo = await uploadPhoto('photo.jpg');
+    obj.user = await createUser('Guillaume', 'Salva');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+  
+  return queue;
+}
