2457c062d46d41d4255b9e1c1ef64d1054b33680
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-31 17:51:47 +0300

    Function to print teachers name

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Defines an interface for object Teacher (Task 1)
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -8,6 +9,17 @@ interface Teacher {
 
 };
 
+// Defines an interface for class Director that extends Object Teacher (Task 2)
 interface Director extends Teacher {
   numberOfReports: number;
 }
+
+// Defines an interface for return type-string (Task 3)
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function to print Initial for firstName and full Lastname (Tasj 3)
+const PrintTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
