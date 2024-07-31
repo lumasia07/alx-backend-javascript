cd46d03b83668f0e3a0e4d34745eff804b54503d
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 00:00:51 +0300

    Brand Convention & Nominal Typing

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,72 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getToWork(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getToWork(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+
+}
+
+export default function createEmployee(salary: number | string): Director | Teacher {
+  const convertSalary = typeof salary === 'string' ? parseInt(salary, 10) : salary;
+
+  if (convertSalary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return (employee as Teacher).workTeacherTasks();
+  }
+}
+
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
