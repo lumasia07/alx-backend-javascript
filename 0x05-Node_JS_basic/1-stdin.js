1202f66ad8805d7a049f692d0ea16a25c271d582
Lumasia <lumasiastancelaus@gmail.com>
2024-09-17 22:13:20 +0300

    fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,17 @@
-console.log('Welcome to Holberton School, what is your name?');
+function stdIn() {
+  console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.setEncoding('utf8');
+  process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.end();
-});
+  process.stdin.on('data', (input) => {
+    const name = input.trim();
+    console.log(`Your name is: ${name}`);
+    process.stdin.end();
+  });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+module.exports = stdIn;
