a12d623a337cbc6af96f9eff7057793a4e5aba5c
Lumasia <lumasiastancelaus@gmail.com>
2024-09-19 20:52:21 +0300

    Spy testing using Sinon

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with "SUM", 100, and 20', function() {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    calculateNumberSpy.restore();
+  });
+});
