f21d7a43c5f3ed8faa54a56332204e96258f2b86
Lumasia <lumasiastancelaus@gmail.com>
2024-09-19 21:23:21 +0300

    Test with hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleSpy;
+
+  beforeEach(function() {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
